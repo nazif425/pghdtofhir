aa1cde43f0d7314ca02be34277ad3a3f9ed2c4c2
Abdullahi Kawu <abdullahiakawu@ibbu.edu.ng>
2025-01-30 17:12:15 +0100

    update code to work with environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ from .models import db
 from .ivr import ivr
 from .wearable import wearable
 from .portal import portal
+#from dotenv import load_dotenv
+#load_dotenv()
 
 def create_app():
     #app = Flask(__name__)
@@ -20,15 +22,16 @@ def create_app():
         __name__,
         template_folder='../templates', 
         static_folder='../static')
-    parentdir = os.path.abspath(os.path.dirname(__file__))
-    rootdir = os.path.dirname(parentdir)
-    sqlite_db_path = os.path.join(rootdir, 'pghdtofhir_db.sqlite')
+    sqlite_db_path = os.path.join(
+        os.path.dirname(os.path.abspath(os.path.dirname(__file__))),
+        os.getenv('SQLITE_DB_PATH')
+    )
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{sqlite_db_path}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
     migrate = Migrate(app, db)
-    app.secret_key = b'\xb7\xc1q\x86\xd3\xf2{5`\xaek\xffV\xea\xf3\x80n\n\xa7\xcb\xc0\x95lo'
+    app.secret_key = os.getenv('SQLITE_DB_PATH')
 
     # Register Blueprints
     app.register_blueprint(ivr, url_prefix='/ivr')
