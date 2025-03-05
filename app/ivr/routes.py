3c775e295d5dede01fd767e8e61f7c8af607ae0e
Nazif <nazifmail2@gmail.com>
2025-03-05 18:37:42 +0100

    Replaced None value with zero or empty string

--- app/ivr/routes.py
+++ app/ivr/routes.py
@@ -305,23 +305,23 @@ def data_request():
     sessions_data = []
     for call_session in call_sessions:
         sessions_data.append({
-            "collection_position": call_session.data.get('collection_position', None),
-            "collection_location": call_session.data.get('collection_location', None),
-            "collection_person": call_session.data.get('collection_person', None),
-            "collection_body_site": call_session.data.get('collection_body_site', None),
+            "collection_position": call_session.data.get('collection_position', ""),
+            "collection_location": call_session.data.get('collection_location', ""),
+            "collection_person": call_session.data.get('collection_person', ""),
+            "collection_body_site": call_session.data.get('collection_body_site', ""),
             "timestamp": call_session.completed_at,
             "new_records": [
                 {
                     'name': "Heart rate", 
-                    'value': call_session.data.get('heart_rate', None)
+                    'value': call_session.data.get('heart_rate', 0)
                 },
                 {
                     'name': "Systolic blood pressure", 
-                    'value': call_session.data.get('systolic_blood_pressure', None)
+                    'value': call_session.data.get('systolic_blood_pressure', 0)
                 },
                 {
                     'name': "Diastolic blood pressure", 
-                    'value': call_session.data.get('diastolic_blood_pressure', None)
+                    'value': call_session.data.get('diastolic_blood_pressure', 0)
                 }
             ]
         })
